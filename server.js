0b8dea7f8b31468444c4de182a70f98ac72086ff
cseibert2667 <cseibert2667@gmail.com>
2020-09-25 08:52:31 -0700

    html routes defined

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const db = require("./models");
+const path = require("path");
 
 const PORT = process.env.PORT || 8080;
 
@@ -17,6 +18,23 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+// html routes 
+app.get("/", (req,res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+})
+
+app.get("/stats", (req,res) => {
+  res.sendFile(path.join(__dirname, "public", "stats.html"))
+})
+
+app.get("/exercise", (req,res) => {
+  res.sendFile(path.join(__dirname, "public", "exercise.html"))
+})
+
+// api routes
+app.get("/api/workouts", (req,res) => {
+  
+})
 
 // Start server
 app.listen(PORT, () => {
