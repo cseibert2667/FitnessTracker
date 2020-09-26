a06a3104ca25a632a2ff6c768ca180fae030e6bd
cseibert2667 <cseibert2667@gmail.com>
2020-09-25 22:24:15 -0700

    edit mongoose connection for heroku deployment

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 // -------- html routes --------
